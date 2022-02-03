10904aebeac1cadba323aff88e97c78495b1f2ea
Coderbro <mo.abbaszadi@gmail.com>
2022-02-03 19:44:28 +0000

    ascii-art-web

--- main.go
+++ main.go
@@ -7,29 +7,29 @@ import (
 	"net/http"
 )
 
-func asciiGen(input string,banner string)string{
+func asciiGen(input string, banner string) string {
 	return input
 }
 
-func ascii(w http.ResponseWriter, r *http.Request){
-	if r.Method == "GET"{
-		tmpl,_ := template.ParseFiles("templates/index.html")
-		tmpl.Execute(w,nil)
+func ascii(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl, _ := template.ParseFiles("templates/index.html")
+		tmpl.Execute(w, nil)
 	} else {
 		r.ParseForm()
 		asciiInput := r.Form["asciiInput"]
 		asciiBanner := r.Form["banner"]
-		fmt.Println(asciiInput,asciiBanner)
-		asciiRaw := asciiGen(asciiInput[0],asciiBanner[0])
-		tmpl,_ := template.ParseFiles("/templates/index.html")
-		tmpl.Execute(w,asciiRaw)
+		fmt.Println(asciiInput, asciiBanner)
+		asciiRaw := asciiGen(asciiInput[0], asciiBanner[0])
+		tmpl, _ := template.ParseFiles("/templates/index.html")
+		tmpl.Execute(w, asciiRaw)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", ascii)
-	err := http.ListenAndServe(":8080",nil)
-	if err != nil{
-		log.Fatalf("500 internal server error : %s",err.Error())
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("500 internal server error : %s", err.Error())
 	}
 }
